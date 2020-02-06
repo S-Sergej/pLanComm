c423db2c97772de5f5c22a7342f79484701d658a
Strapak, Sergej <strapak@mediamarktsaturn.com>
2020-02-06 17:17:31 +0100

    Annoying build stuff

---
+++ client/build/precache-manifest.b387217415e2ccbf18ab29e75ac09f9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8db5cee78cb0d7c72e113a9618626795",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71a391a609fb963ab359",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "3ee42c48e98049bfc913",
+    "url": "/static/css/main.b754237b.chunk.css"
+  },
+  {
+    "revision": "71a391a609fb963ab359",
+    "url": "/static/js/2.fe000d5b.chunk.js"
+  },
+  {
+    "revision": "db1f62499706d43bc6518ddbe02ac5ce",
+    "url": "/static/js/2.fe000d5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ee42c48e98049bfc913",
+    "url": "/static/js/main.841fb5cd.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
