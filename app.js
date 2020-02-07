c43146919e30e042a286308b42cedd81039f8dba
Strapak, Sergej <strapak@mediamarktsaturn.com>
2020-02-07 11:38:42 +0100

    adding whole client directory to static path

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(
 
 //app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client/build", "client")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // ADD SESSION SETTINGS HERE:
