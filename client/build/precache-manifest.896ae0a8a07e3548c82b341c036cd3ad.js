d75b6a0106d67733b6be2a2bcb75e6af95d9bb48
Strapak, Sergej <strapak@mediamarktsaturn.com>
2020-02-11 15:56:06 +0100

    merge with builds

---
+++ client/build/precache-manifest.896ae0a8a07e3548c82b341c036cd3ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a57953a7204750e7530d66da121bc14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e64109219f25f305413",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "e4925fed0f19e697f9f8",
+    "url": "/static/css/main.107db63e.chunk.css"
+  },
+  {
+    "revision": "4e64109219f25f305413",
+    "url": "/static/js/2.9754e2e1.chunk.js"
+  },
+  {
+    "revision": "db1f62499706d43bc6518ddbe02ac5ce",
+    "url": "/static/js/2.9754e2e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4925fed0f19e697f9f8",
+    "url": "/static/js/main.71c3c2b5.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
