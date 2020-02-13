72da06024176c8af393b476ee2f19b36c739316b
kilianal <kilianal@mediamarktsaturn.com>
2020-02-13 15:42:19 +0100

    styling mainpage and evendtsdetails finished

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -3,13 +3,17 @@ import Event from './Event'
 import Players from './Players'
 import { Transition, animated } from 'react-spring/renderprops'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Modal from 'react-bootstrap/Modal';
+import {Spring} from 'react-spring/renderprops'
 import axios from 'axios'
+
 
 export default class MainPage extends Component {
   state = {
     events : [],
     showDiv: true,
-    buttonText: false
+    buttonText: false,
+    show: false,
   }
 
   showDiv = () => {
@@ -32,36 +36,48 @@ export default class MainPage extends Component {
     this.getAllEvents()
   }
 
+
+  //Methodas for the modal
+  handleClose = () => {
+    this.setState({
+      show: false,
+      title: "",
+      description: "",
+      
+    })
+  };
+  handleShow = () => {
+    this.setState({
+      show: true
+    })
+  };
+
+
+
   render () {
     return (
-      <div>
-        <Event allEvents={() => this.getAllEvents()} />
-        <div className='gameArrange'>
-          {this.state.events.map(oneEvent => (
-            <a href= {`/event/${oneEvent._id}`} >
-            <div key={oneEvent._id} >
-              <div className='EventData'>
-                <div>
-                <p><h3>{new Date(oneEvent.eventdate).toLocaleDateString()}</h3></p>
-                  <h1>{oneEvent.eventname}</h1>
-                  <p>Start at: {new Date(oneEvent.eventdate).toLocaleTimeString()}</p>
-                  <p>participants: {oneEvent.subscriber.length}</p>
-                </div>
-              </div>
-            </div>
-            </a>
-          ))}
-        </div>
-        <div className='alignPlayerShow'>
+      <div className="Mainpage">
+      
+      {/* Hier ist die Sidebar für die Anzeige der registrierten User */}
+      <div className='alignPlayerShow'>
           {this.state.buttonText ? (
+            <Spring
+            from={{opacity: 0.3}}
+            to={{opacity:1}}
+            config={{duration: 600}}>
+              {props => <div style={props}>
             <button className='userButton' onClick={this.showDiv}>
               Show
               <FontAwesomeIcon icon='users' />
             </button>
+            </div>}
+            </Spring>
           ) : (
-            <button onClick={this.showDiv}>
+            
+            <button className='userButton hide' onClick={this.showDiv}>
               Hide <FontAwesomeIcon icon='users' />
             </button>
+            
           )}
           <Transition
             native
@@ -81,6 +97,41 @@ export default class MainPage extends Component {
             }
           </Transition>
         </div>
+      
+        {/* Modal für das Formular zum Erstellen von Events */}
+        <div className="event_basic">
+        <button  className="btn_event" variant="primary" onClick={this.handleShow}><FontAwesomeIcon icon="pen" /> Create Event  </button>
+      <Modal style={{marginTop: "200px"}} show={this.state.show} onHide={this.handleClose} animation={true}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create Event</Modal.Title>
+        </Modal.Header>
+          <Modal.Body >
+              {/* Hier ist das Formular für die Events */}
+          <Event allEvents={() => this.getAllEvents()} handleCloseModal={this.handleClose}/>
+         </Modal.Body>
+        
+      </Modal>
+      
+
+       {/* Hier stehen dann die Events */}
+        <div className='eventArrange'>
+      
+          {this.state.events.map(oneEvent => (
+            <div key={oneEvent._id} className="singleEvent">
+            <a href= {`/event/${oneEvent._id}`} >
+            
+                  <h3>{new Date(oneEvent.eventdate).toLocaleDateString()}</h3>
+                  <h1>{oneEvent.eventname}</h1>
+                  <p>Start at: {new Date(oneEvent.eventdate).toLocaleTimeString()}</p>
+                  <p>participants: {oneEvent.subscriber.length}</p>
+           
+          </a>
+          </div>
+          ))}
+        </div>
+        </div>
+
+        
       </div>
     )
   }
